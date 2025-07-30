c619ce8f7f8205eab54eb270fb9f4cb6b1efccd5
cstancu-wb <clara.stancu@whitbread.com>
2025-07-30 12:43:19 +0300

    made is springBootApplication

---
+++ src/main/java/org/example/memeoverflow/MemeOverflowServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.memeoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeOverflowServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MemeOverflowServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/memeoverflow/MemeOverflowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.memeoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeOverflowServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
